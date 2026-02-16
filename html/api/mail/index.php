71db591f8263d304b101ea0e50b295036eb338de
Ameilia <ameiliamerhair@gmail.com>
2026-02-15 20:57:47 -0500

    Commit 3
    Up until step 6.

--- html/api/mail/index.php
+++ html/api/mail/index.php
@@ -1,9 +1,29 @@
 <?php
-declare(strict_types=1);
+require '../../vendor/autoload.php';
 
-echo "<h1>SupportDesk Demo</h1>";
-echo "<p>PHP + Apache container is running.</p>";
-echo "<p>Next step: add REST endpoints + Composer + PHPUnit.</p>";
-echo "";
+use Application\Mail;
+use Application\Page;
 
-// phpinfo();
+$dsn = "pgsql:host=" . getenv('DB_PROD_HOST') . ";dbname=" . getenv('DB_PROD_NAME');
+try {
+    $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'), [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo "Database connection failed: " . $e->getMessage();
+    exit;
+}
+
+$mail = new Mail($pdo);
+$page = new Page();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $json = file_get_contents("php://input");
+    $data = json_decode($json, true);
+
+    $page->item($mail->createMail($data['subject'], $data['body']));
+    exit;
+}
+
+$page->badRequest();
--- init-prod/init.sql
+++ init-prod/init.sql
@@ -1,6 +1,9 @@
-CREATE TABLE IF NOT EXISTS demo_ping(
+CREATE TABLE IF NOT EXISTS mail(
     id SERIAL PRIMARY KEY,
-    message TEXT NOT NULL
+    subject TEXT NOT NULL,
+    body TEXT NOT NULL,
 );
 
-INSERT INTO demo_ping (message) VALUES ('Postgres is up!');
+INSERT INTO mail (subject, body) 
+VALUES ('Welcome! This is your first mail.')
+ON CONFLICT DO NOTHING;
